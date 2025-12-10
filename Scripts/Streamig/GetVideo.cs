f36bfa3a1bf9042f2d4c36b475b3dde4f5f69bfb
Alikhan Khamidulla <88340494+ALHNk@users.noreply.github.com>
2025-12-10 09:51:56 +0500

    Refactor GetVideo class To try Fix Mixing of cameras

--- Scripts/Streamig/GetVideo.cs
+++ Scripts/Streamig/GetVideo.cs
@@ -1,110 +1,126 @@
-﻿using System;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using UnityEngine;
-using UnityEngine.UI;
-using PimDeWitte.UnityMainThreadDispatcher;
 
 public class GetVideo : MonoBehaviour
 {
-	public string host = "10.224.123.82";
-	public int port = 12345;
+    public string host = "10.224.123.82";
+    public int port = 12345;
 
-	//public RawImage fish;
-	public Material display;
+    public Material display;
 
-	private UdpClient udpClient;
+    private UdpClient udpClient;
 
-	private byte[] frameBuffer = new byte[5 * 1024 * 1024];
-	private int framePos = 0;
-	private bool foundHeader = false;
+    private byte[] frameBuffer = new byte[5 * 1024 * 1024];
+    private int framePos = 0;
+    private bool foundHeader = false;
 
-	private FishEyeCon fishEyeCon;
+    private FishEyeCon fishEyeCon;
 
-	void Start()
-	{
-		fishEyeCon = GetComponentInChildren<FishEyeCon>();
+    private Queue<byte[]> queue = new Queue<byte[]>();
+    private object queueLock = new object();
 
-		udpClient = new UdpClient(port);   // bind to listen port
-		udpClient.Client.ReceiveBufferSize = 5 * 1024 * 1024;
+    void Start()
+    {
+        fishEyeCon = GetComponentInChildren<FishEyeCon>();
 
-		new Thread(ReceiveLoop).Start();
-	}
+        udpClient = new UdpClient(port);
+        udpClient.Client.ReceiveBufferSize = 5 * 1024 * 1024;
 
-	void ReceiveLoop()
-	{
-		IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+        Thread t = new Thread(ReceiveLoop);
+        t.IsBackground = true;
+        t.Start();
+    }
 
-		try
-		{
-			while (true)
-			{
-				byte[] packet = udpClient.Receive(ref remoteEP);
+    void ReceiveLoop()
+    {
+        IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
 
-				// Process the entire UDP packet
-				for (int i = 0; i < packet.Length; i++)
-				{
-					byte b = packet[i];
+        try
+        {
+            while (true)
+            {
+                byte[] packet = udpClient.Receive(ref remoteEP);
 
-					if (!foundHeader)
-					{
-						// Search for JPEG Start FFD8
-						if (i > 0 && packet[i - 1] == 0xFF && b == 0xD8)
-						{
-							foundHeader = true;
-							framePos = 0;
-							frameBuffer[framePos++] = 0xFF;
-							frameBuffer[framePos++] = 0xD8;
-						}
-					}
-					else
-					{
-						frameBuffer[framePos++] = b;
+                for (int i = 0; i < packet.Length; i++)
+                {
+                    byte b = packet[i];
 
-						// Detect JPEG End FFD9
-						if (framePos > 2 &&
-							frameBuffer[framePos - 2] == 0xFF &&
-							frameBuffer[framePos - 1] == 0xD9)
-						{
-							byte[] jpegData = new byte[framePos];
-							Array.Copy(frameBuffer, jpegData, framePos);
+                    if (!foundHeader)
+                    {
+                        if (i > 0 && packet[i - 1] == 0xFF && b == 0xD8)
+                        {
+                            foundHeader = true;
+                            framePos = 0;
+                            frameBuffer[framePos++] = 0xFF;
+                            frameBuffer[framePos++] = 0xD8;
+                        }
+                    }
+                    else
+                    {
+                        frameBuffer[framePos++] = b;
 
-							foundHeader = false;
-							framePos = 0;
+                        if (framePos > 2 &&
+                            frameBuffer[framePos - 2] == 0xFF &&
+                            frameBuffer[framePos - 1] == 0xD9)
+                        {
+                            byte[] jpegData = new byte[framePos];
+                            Array.Copy(frameBuffer, jpegData, framePos);
 
-							UpdateFrame(jpegData);
-						}
-					}
-				}
-			}
-		}
-			catch (Exception e)
-			{
-				Debug.LogError("UDP Receive Error: " + e.Message);
-			}
-	}
+                            foundHeader = false;
+                            framePos = 0;
 
-	void UpdateFrame(byte[] data)
-	{
-		UnityMainThreadDispatcher.Instance().Enqueue(() =>
-		{
-			Texture2D tex = new Texture2D(2, 2, TextureFormat.RGB24, false);
+                            // кладём кадр в собственную очередь
+                            lock (queueLock)
+                            {
+                                queue.Enqueue(jpegData);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("UDP Receive Error: " + e.Message);
+        }
+    }
 
-			if (tex.LoadImage(data))
-			{
-				Texture2D converted = fishEyeCon.Convert(tex);
-				display.SetTexture("_BaseMap", converted);			
-			}
-			else
-			{
-				Debug.LogWarning("Bad JPEG frame");
-			}
-		});
-	}
+    void Update()
+    {
+        byte[] frame = null;
 
-	void OnApplicationQuit()
-	{
-		udpClient?.Close();
-	}
+        lock (queueLock)
+        {
+            if (queue.Count > 0)
+                frame = queue.Dequeue();
+        }
+
+        if (frame != null)
+        {
+            ApplyFrame(frame);
+        }
+    }
+
+    void ApplyFrame(byte[] data)
+    {
+        Texture2D tex = new Texture2D(2, 2, TextureFormat.RGB24, false);
+
+        if (tex.LoadImage(data))
+        {
+            Texture2D converted = fishEyeCon.Convert(tex);
+            display.SetTexture("_BaseMap", converted);
+        }
+        else
+        {
+            Debug.LogWarning("Bad JPEG frame");
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        udpClient?.Close();
+    }
 }
